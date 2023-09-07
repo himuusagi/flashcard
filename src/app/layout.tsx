008c2609b4bf60bb7e11d8defb4c7aa75faf047e
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-07 13:43:49 +0900

    TypeScript用のESLint設定、Prettierが整形した箇所にエラーを出さない設定、ESLintの推奨のルールを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import { type ReactNode } from 'react'
+import {type Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
