55e3a2ed7fb15c7e26057ab9cdd261b41731c49d
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-08 20:17:39 +0900

    tailwind.cssのカラーパレットを拡張

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: { light: "#90DDDD", DEFAULT: "#22BABB", dark: "#348888" },
+        textBlack: "#333333",
+        gray: "#7E7E7E",
+      },
     },
   },
   plugins: [],
