180a91c759f653f6dadafbdd0604a2122e8e07dd
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-10 13:13:22 +0900

    titleプロップスのスペース２個を改行して表示させる

--- src/components/layouts/Heading.tsx
+++ src/components/layouts/Heading.tsx
@@ -5,16 +5,26 @@ type Props = {
 };
 
 const Heading: FC<Props> = ({ title }) => {
+  const lineBrokenTitle = (
+    <>
+      {title.split("  ").map((text, index) => {
+        return (
+          <span key={index} className="block">
+            {text}
+          </span>
+        );
+      })}
+    </>
+  );
+
   return (
     <div className="flex justify-center">
-      <div className="relative min-w-[280px] text-center px-[40px] py-[8px]">
-        <h1>
-          <span className="text-primary text-2xl">{title}</span>
-        </h1>
-        <div className=" border border-transparent border-t-primary border-l-primary absolute left-0 top-0 h-[10px] w-[10px] " />
-        <div className=" border border-transparent border-b-primary border-l-primary absolute left-0 bottom-0 h-[10px] w-[10px] " />
-        <div className=" border border-transparent border-t-primary border-r-primary absolute right-0 top-0 h-[10px] w-[10px] " />
-        <div className=" border border-transparent border-b-primary border-r-primary absolute right-0 bottom-0 h-[10px] w-[10px] " />
+      <div className="relative min-w-[280px] px-[40px] py-[8px] text-center">
+        <h1 className="text-2xl text-primary">{lineBrokenTitle}</h1>
+        <div className=" absolute left-0 top-0 h-[10px] w-[10px] border border-transparent border-l-primary border-t-primary " />
+        <div className=" absolute bottom-0 left-0 h-[10px] w-[10px] border border-transparent border-b-primary border-l-primary " />
+        <div className=" absolute right-0 top-0 h-[10px] w-[10px] border border-transparent border-r-primary border-t-primary " />
+        <div className=" absolute bottom-0 right-0 h-[10px] w-[10px] border border-transparent border-b-primary border-r-primary " />
       </div>
       <div />
     </div>
