78628059d12de5c054ef75ac89e58fb35c6f394d
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-12 12:48:20 +0900

    EllipsisButtonの修正
    プロップスでクラス名を与えられるようにした
    ホバー時、フォーカス時のアニメーションを加えた

--- src/components/elements/EllipsisButton.tsx
+++ src/components/elements/EllipsisButton.tsx
@@ -2,17 +2,18 @@ import { type FC, type MouseEventHandler } from "react";
 
 type Props = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
 };
 
-const EllipsisButton: FC<Props> = ({ onClick }) => {
+const EllipsisButton: FC<Props> = ({ onClick, className }) => {
   return (
     <button
       onClick={onClick}
-      className="relative w-[32px] rounded-full border border-primary before:block before:pt-[100%]"
+      className={`group relative w-[32px] rounded-full border border-primary duration-100 before:block before:pt-[100%] hover:bg-primary focus:outline-primary-dark ${className}`}
     >
-      <span className="absolute left-[25%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary before:block before:pt-[100%]" />
-      <span className="absolute left-[50%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary before:block before:pt-[100%]" />
-      <span className="absolute left-[75%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary before:block before:pt-[100%]" />
+      <span className="absolute left-[25%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary duration-100 before:block before:pt-[100%] group-hover:bg-white" />
+      <span className="absolute left-[50%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary duration-100 before:block before:pt-[100%] group-hover:bg-white" />
+      <span className="absolute left-[75%] top-[50%] block w-[4px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary duration-100 before:block before:pt-[100%] group-hover:bg-white" />
     </button>
   );
 };
