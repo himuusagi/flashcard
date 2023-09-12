8f620be543ea0fec2d5b17ad0a2326beb3b6a630
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-12 12:55:48 +0900

    LinkedTextコンポーネントの作成

---
+++ src/components/elements/LinkedText.tsx
@@ -0,0 +1,20 @@
+import { type FC } from "react";
+import Link from "next/link";
+
+type Props = {
+  text: string;
+  className?: string;
+};
+
+const LinkedText: FC<Props> = ({ text, className }) => {
+  return (
+    <Link
+      href=""
+      className={`relative text-primary before:absolute before:bottom-0 before:left-0 before:block  before:h-[1px] before:w-full before:bg-transparent before:duration-100 hover:before:bg-primary focus:outline-primary-light ${className}`}
+    >
+      {text}
+    </Link>
+  );
+};
+
+export default LinkedText;
