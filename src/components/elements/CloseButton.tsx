fdb9c39d7216337b40ea386f62b2d75856b0b23d
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-12 12:53:09 +0900

    CloseButtonの修正
    ホバー時、フォーカス時のアニメーションを追加

--- src/components/elements/CloseButton.tsx
+++ src/components/elements/CloseButton.tsx
@@ -2,16 +2,17 @@ import { type FC, type MouseEventHandler } from "react";
 
 type Props = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
 };
 
-const CloseButton: FC<Props> = ({ onClick }) => {
+const CloseButton: FC<Props> = ({ onClick, className }) => {
   return (
     <button
       onClick={onClick}
-      className="relative w-[32px] rounded-full border border-primary before:block before:pt-[100%]"
+      className={`group relative w-[32px] rounded-full border border-primary duration-100 before:block before:pt-[100%] hover:bg-primary focus:outline-primary-dark ${className}`}
     >
-      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[45deg] bg-primary" />
-      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] bg-primary" />
+      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[45deg] bg-primary duration-100 group-hover:bg-white" />
+      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] bg-primary duration-100 group-hover:bg-white" />
     </button>
   );
 };
