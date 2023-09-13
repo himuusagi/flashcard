8f721c7c9f1358957530534a1bab242c25200b1e
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-13 16:06:39 +0900

    CloseButtonコンポーネントの修正
    sizeプロパティで大きさを選択できるようにした

--- src/components/elements/CloseButton.tsx
+++ src/components/elements/CloseButton.tsx
@@ -1,18 +1,39 @@
 import { type FC, type MouseEventHandler } from "react";
 
 type Props = {
+  size?: "medium" | "small";
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
 };
 
-const CloseButton: FC<Props> = ({ onClick, className }) => {
+const CloseButton: FC<Props> = ({ size, onClick, className }) => {
+  let styleOfCircleSize = "";
+  let styleOfDotSize = "";
+  switch (size) {
+    case "medium":
+      styleOfCircleSize = "w-[32px]";
+      styleOfDotSize = "w-[16px]";
+      break;
+    case "small":
+      styleOfCircleSize = "w-[24px]";
+      styleOfDotSize = "w-[12px]";
+      break;
+    default:
+      styleOfCircleSize = "w-[32px]";
+      styleOfDotSize = "w-[16px]";
+  }
+
   return (
     <button
       onClick={onClick}
-      className={`group relative w-[32px] rounded-full border border-primary duration-100 before:block before:pt-[100%] hover:bg-primary focus:outline-primary-dark ${className}`}
+      className={`group relative rounded-full border border-primary duration-100 before:block before:pt-[100%] hover:bg-primary focus:outline-primary-dark ${className} ${styleOfCircleSize}`}
     >
-      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[45deg] bg-primary duration-100 group-hover:bg-white" />
-      <span className="absolute left-[50%] top-[50%] block h-[1px] w-[16px] -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] bg-primary duration-100 group-hover:bg-white" />
+      <span
+        className={`absolute left-[50%] top-[50%] block h-[1px] -translate-x-1/2 -translate-y-1/2 rotate-[45deg] bg-primary duration-100 group-hover:bg-white ${styleOfDotSize}`}
+      />
+      <span
+        className={`absolute left-[50%] top-[50%] block h-[1px] -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] bg-primary duration-100 group-hover:bg-white ${styleOfDotSize}`}
+      />
     </button>
   );
 };
