7ab27026236148bc345f617dc7a76143783356f8
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-24 18:06:51 +0900

    QAndAAccordionコンポーネントの修正
    questionTextプロップスの改行コード`\n`で`<br />`を挿入するようにした
    answerTextプロップスの改行コード`\n`で区切って、pタグで囲むようにした

--- src/components/layouts/QAndAAccordion.tsx
+++ src/components/layouts/QAndAAccordion.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { type FC, type Dispatch, type SetStateAction } from "react";
+import {
+  type FC,
+  type ReactNode,
+  type ReactElement,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 
 type Props = {
   questionText: string;
@@ -10,28 +16,29 @@ type Props = {
 };
 
 const QAndAAccordion: FC<Props> = ({ questionText, answerText, isOpen, setIsOpen }) => {
-  const splittedAnswerTexts = answerText.split("  ");
-  const accordionContent = (
-    <>
-      {splittedAnswerTexts.map((text, index) => {
-        return (
-          <p
-            key={index}
-            className={`duration-300 ${
-              isOpen ? "mt-2 leading-[1.5] first:mt-0" : "mt-0 leading-[0]"
-            }`}
-          >
-            {text}
-          </p>
-        );
-      })}
-    </>
-  );
+  const lineBrokenQuestionTexts: ReactNode[] = [];
+  questionText.split(/\n/).forEach((text, index) => {
+    if (index !== 0) {
+      lineBrokenQuestionTexts.push(<br key={index} />);
+    }
+    lineBrokenQuestionTexts.push(text);
+  });
+
+  const lineBrokenAnswerTexts: ReactElement[] = answerText.split(/\n/).map((text, index) => {
+    return (
+      <p
+        key={index}
+        className={`duration-300 ${isOpen ? "mt-2 leading-[1.5] first:mt-0" : "mt-0 leading-[0]"}`}
+      >
+        {text}
+      </p>
+    );
+  });
 
   return (
     <div className="w-full">
       <div onClick={() => setIsOpen((prev) => !prev)} className="cursor-pointer">
-        <p>{questionText}</p>
+        <p>{lineBrokenQuestionTexts}</p>
       </div>
 
       <div
@@ -39,7 +46,7 @@ const QAndAAccordion: FC<Props> = ({ questionText, answerText, isOpen, setIsOpen
           isOpen ? "mt-[12px] leading-[1.5] opacity-100" : "mt-0 leading-[0] opacity-0"
         }`}
       >
-        {accordionContent}
+        {lineBrokenAnswerTexts}
       </div>
     </div>
   );
