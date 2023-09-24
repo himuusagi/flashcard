9f1f2167cedb840f3f75f2834fefc9c0ae93a7ca
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-24 17:37:25 +0900

    Headingコンポーネントの修正
    titleプロップスの改行コード`\n`で`<br />`が挿入されるようにした

--- src/components/layouts/Heading.tsx
+++ src/components/layouts/Heading.tsx
@@ -1,26 +1,23 @@
-import { type FC } from "react";
+import { type FC, type ReactNode } from "react";
 
 type Props = {
   title: string;
 };
 
 const Heading: FC<Props> = ({ title }) => {
-  const lineBrokenTitle = (
-    <>
-      {title.split("  ").map((text, index) => {
-        return (
-          <span key={index} className="block">
-            {text}
-          </span>
-        );
-      })}
-    </>
-  );
+  const lineBrokenTexts: ReactNode[] = [];
+
+  title.split(/\n/).forEach((text, index) => {
+    if (index !== 0) {
+      lineBrokenTexts.push(<br key={index} />);
+    }
+    lineBrokenTexts.push(text);
+  });
 
   return (
     <div className="flex justify-center">
       <div className="relative min-w-[280px] px-[40px] py-[8px] text-center">
-        <h1 className="text-2xl text-primary">{lineBrokenTitle}</h1>
+        <h1 className="text-2xl text-primary">{lineBrokenTexts}</h1>
         <div className=" absolute left-0 top-0 h-[10px] w-[10px] border border-transparent border-l-primary border-t-primary " />
         <div className=" absolute bottom-0 left-0 h-[10px] w-[10px] border border-transparent border-b-primary border-l-primary " />
         <div className=" absolute right-0 top-0 h-[10px] w-[10px] border border-transparent border-r-primary border-t-primary " />
