316a01bfe7f68deae323c5b8767f577d8286b313
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-25 17:25:09 +0900

    TextAreaコンポーネントの修正
    colorプロップスを追加
    プロップスの値に応じてカラーを変更するようにした

--- src/components/elements/TextArea.tsx
+++ src/components/elements/TextArea.tsx
@@ -4,9 +4,37 @@ import { useState, useRef, type FC, type ChangeEventHandler } from "react";
 
 type Props = {
   placeholder?: string;
+  color?: "primary" | "gray";
 };
 
-const TextArea: FC<Props> = ({ placeholder }) => {
+const TextArea: FC<Props> = ({ placeholder, color }) => {
+  let textAreaStyleOfColor: string;
+  let labelStyleOfColor: string;
+  let focusedLabelStyleOfColor: string;
+  let borderStyleOfColor: string;
+  switch (color) {
+    case "primary": {
+      textAreaStyleOfColor = "text-primary placeholder-primary-light focus:text-primary-dark";
+      labelStyleOfColor = "peer-focus:text-primary-dark";
+      focusedLabelStyleOfColor = "text-primary";
+      borderStyleOfColor = "bg-primary peer-focus:bg-primary-dark";
+      break;
+    }
+    case "gray": {
+      textAreaStyleOfColor = "text-gray placeholder-gray-light focus:text-gray-dark";
+      labelStyleOfColor = "peer-focus:text-gray-dark";
+      focusedLabelStyleOfColor = "text-gray";
+      borderStyleOfColor = "bg-gray peer-focus:bg-gray-dark";
+      break;
+    }
+    default: {
+      textAreaStyleOfColor = "text-primary placeholder-primary-light focus:text-primary-dark";
+      labelStyleOfColor = "peer-focus:text-primary-dark";
+      focusedLabelStyleOfColor = "text-primary";
+      borderStyleOfColor = "bg-primary peer-focus:bg-primary-dark";
+    }
+  }
+
   const [value, setValue] = useState("");
 
   const textarea = useRef<HTMLTextAreaElement | null>(null);
@@ -28,18 +56,24 @@ const TextArea: FC<Props> = ({ placeholder }) => {
         placeholder={placeholder}
         onChange={onChange}
         rows={1}
-        className={`peer h-[32px] min-h-[32px] w-full resize-none overflow-hidden px-[32px] py-[4px] text-primary placeholder-primary-light outline-none duration-150 focus:text-primary-dark focus:placeholder-transparent`}
+        className={`peer h-[32px] min-h-[32px] w-full resize-none overflow-hidden px-[32px] py-[4px] outline-none duration-150 focus:placeholder-transparent ${textAreaStyleOfColor}`}
       />
       <span
-        className={`absolute left-[32px] top-[-10px] text-[10px] duration-150 peer-focus:text-primary-dark
-        ${value.length > 0 ? "text-primary focus:text-primary-dark" : "text-transparent"}
+        className={`absolute left-[32px] top-[-10px] text-[10px] duration-150 ${labelStyleOfColor}
+        ${value.length > 0 ? focusedLabelStyleOfColor : "text-transparent"}
         `}
       >
         {placeholder}
       </span>
-      <div className="absolute bottom-1 left-0 h-[1px] w-full bg-primary duration-150 peer-focus:bg-primary-dark" />
-      <div className="absolute bottom-1 left-0 h-[10px] w-[1px] bg-primary duration-150 peer-focus:bg-primary-dark" />
-      <div className="absolute bottom-1 right-0 h-[10px] w-[1px] bg-primary duration-150 peer-focus:bg-primary-dark" />
+      <div
+        className={`absolute bottom-1 left-0 h-[1px] w-full  duration-150 ${borderStyleOfColor}`}
+      />
+      <div
+        className={`absolute bottom-1 left-0 h-[10px] w-[1px] duration-150 ${borderStyleOfColor}`}
+      />
+      <div
+        className={`absolute bottom-1 right-0 h-[10px] w-[1px] duration-150 ${borderStyleOfColor}`}
+      />
     </div>
   );
 };
