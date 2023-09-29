797412223860ef8cef9d2669d5bb6720edf71df0
MizukiNakamura <himuusagi1127@gmail.com>
2023-09-29 15:43:43 +0900

    BoxWithoutTitleコンポーネントの修正
    プロップスでborderカラーを変えられるようにした

--- src/components/layouts/BoxWithoutTitle.tsx
+++ src/components/layouts/BoxWithoutTitle.tsx
@@ -2,11 +2,27 @@ import { type FC, type ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  borderColor?: "primary" | "gray";
 };
 
-const CardWithoutTitle: FC<Props> = ({ children }) => {
+const CardWithoutTitle: FC<Props> = ({ children, borderColor }) => {
+  let styleOfColor: string;
+  switch (borderColor) {
+    case "primary": {
+      styleOfColor = "border-primary";
+      break;
+    }
+    case "gray": {
+      styleOfColor = "border-gray";
+      break;
+    }
+    default: {
+      styleOfColor = "border-primary";
+    }
+  }
+
   return (
-    <div className="rounded-[4px] border border-primary py-[8px] pl-[16px] pr-[8px]">
+    <div className={`rounded-[4px] border py-[8px] pl-[16px] pr-[8px] ${styleOfColor}`}>
       {children}
     </div>
   );
