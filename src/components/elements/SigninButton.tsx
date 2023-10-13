5c53ab466ea8ab809d46a639c8aca40688885006
MizukiNakamura <himuusagi1127@gmail.com>
2023-10-13 18:45:56 +0900

    サインインボタンコンポーネントの修正
    buttonタグを使って作っていたところを、Buttonコンポーネントを再利用して作るようにした。

--- src/components/elements/SigninButton.tsx
+++ src/components/elements/SigninButton.tsx
@@ -2,17 +2,16 @@
 
 import { type FC } from "react";
 import { signIn } from "next-auth/react";
+import Button from "./Button";
 
 const SigninButton: FC = () => {
   return (
-    <button
+    <Button
       type="button"
+      text="Googleでサインイン"
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onClick={() => signIn("google", { callbackUrl: "/" })}
-      className="min-w-[200px] rounded-[4px]  border border-primary px-[40px] py-[4px] text-center text-xl text-primary outline-none duration-100 hover:bg-primary hover:text-white focus:shadow focus:shadow-primary"
-    >
-      Googleでサインイン
-    </button>
+    />
   );
 };
 
