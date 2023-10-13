c0ec9c3dd14665a07a2952a92d2b30ed290e4375
MizukiNakamura <himuusagi1127@gmail.com>
2023-10-13 18:43:23 +0900

    SignoutButtonコンポーネントの作成
    `src/components/elements/SignoutButton.tsx`の追加

---
+++ src/components/elements/SignoutButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { type FC } from "react";
+import { signOut } from "next-auth/react";
+import Button from "./Button";
+
+const SignoutButton: FC = () => {
+  return (
+    <Button
+      type="button"
+      text="サインアウト"
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      onClick={() => signOut({ callbackUrl: "/signin" })}
+    />
+  );
+};
+
+export default SignoutButton;
