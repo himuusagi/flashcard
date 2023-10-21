6cf7a5b371ae0de821a10a4a4f25abbf7865a9ec
MizukiNakamura <himuusagi1127@gmail.com>
2023-10-21 20:02:26 +0900

    ESLintのエラーの無視

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-/* eslint-disable*/
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
-/* eslint-disable*/
 
 export { handler as GET, handler as POST };
