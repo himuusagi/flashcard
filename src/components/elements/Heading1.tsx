bd4fe321cc88f188c6d19564dd191a3c01a7fbf8
MizukiNakamura <himuusagi1127@gmail.com>
2023-10-25 18:10:34 +0900

    Heading1コンポーネントの修正
    colorプロップスを渡せるようにし、コンポーネントのカラーを決められるようにした

--- src/components/elements/Heading1.tsx
+++ src/components/elements/Heading1.tsx
@@ -2,9 +2,29 @@ import { type FC, type ReactNode } from "react";
 
 type Props = {
   title: string;
+  color?: "primary" | "accent";
 };
 
-const Heading1: FC<Props> = ({ title }) => {
+const Heading1: FC<Props> = ({ title, color = "primary" }) => {
+  let styleOfTextColor: string;
+  let styleOfBorderColor: string;
+  switch (color) {
+    case "primary": {
+      styleOfTextColor = "text-primary";
+      styleOfBorderColor = "border-primary";
+      break;
+    }
+    case "accent": {
+      styleOfTextColor = "text-accent";
+      styleOfBorderColor = "border-accent";
+      break;
+    }
+    default: {
+      const wrongColor: never = color;
+      throw new Error(`${wrongColor as string} is wrong type.`);
+    }
+  }
+
   const lineBrokenTexts: ReactNode[] = [];
 
   title.split(/\n/).forEach((text, index) => {
@@ -17,11 +37,19 @@ const Heading1: FC<Props> = ({ title }) => {
   return (
     <div className="mb-[40px] flex justify-center">
       <div className="relative min-w-[280px] px-[40px] py-[8px] text-center">
-        <h1 className="text-2xl text-primary">{lineBrokenTexts}</h1>
-        <div className=" absolute left-0 top-0 h-[10px] w-[10px] border border-transparent border-l-primary border-t-primary " />
-        <div className=" absolute bottom-0 left-0 h-[10px] w-[10px] border border-transparent border-b-primary border-l-primary " />
-        <div className=" absolute right-0 top-0 h-[10px] w-[10px] border border-transparent border-r-primary border-t-primary " />
-        <div className=" absolute bottom-0 right-0 h-[10px] w-[10px] border border-transparent border-b-primary border-r-primary " />
+        <h1 className={`text-2xl text-primary ${styleOfTextColor}`}>{lineBrokenTexts}</h1>
+        <div
+          className={`absolute left-0 top-0 h-[10px] w-[10px] border-l border-t ${styleOfBorderColor}`}
+        />
+        <div
+          className={`absolute bottom-0 left-0 h-[10px] w-[10px] border-b border-l ${styleOfBorderColor}`}
+        />
+        <div
+          className={`absolute right-0 top-0 h-[10px] w-[10px] border-r border-t ${styleOfBorderColor}`}
+        />
+        <div
+          className={`absolute bottom-0 right-0 h-[10px] w-[10px] border-b border-r ${styleOfBorderColor}`}
+        />
       </div>
       <div />
     </div>
