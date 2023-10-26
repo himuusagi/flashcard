55812c21b5412e0b277ed18087f2af639ed2650f
MizukiNakamura <himuusagi1127@gmail.com>
2023-10-26 13:31:46 +0900

    BoxWithTitleコンポーネントの修正
    colorプロップスに`"accent"`を追加

--- src/components/layouts/BoxWithTitle.tsx
+++ src/components/layouts/BoxWithTitle.tsx
@@ -3,10 +3,10 @@ import { type FC, type ReactElement } from "react";
 type Props = {
   title: string;
   text: string;
-  color?: "primary" | "dark" | "black";
+  color?: "primary" | "dark" | "black" | "accent";
 };
 
-const BoxWithTitle: FC<Props> = ({ title, text, color }) => {
+const BoxWithTitle: FC<Props> = ({ title, text, color = "primary" }) => {
   const lineBrokenTexts: ReactElement[] = text.split(/\n/).map((text, index) => {
     return (
       <p key={index} className="mt-2 first:mt-0">
@@ -15,34 +15,39 @@ const BoxWithTitle: FC<Props> = ({ title, text, color }) => {
     );
   });
 
-  let borderStyleOfColor: string;
-  let textStyleOfColor: string;
+  let styleOfBorderColor: string;
+  let styleOfTextColor: string;
   switch (color) {
     case "primary": {
-      borderStyleOfColor = "border-primary";
-      textStyleOfColor = "text-primary";
+      styleOfBorderColor = "border-primary";
+      styleOfTextColor = "text-primary";
       break;
     }
     case "dark": {
-      borderStyleOfColor = "border-primary-dark";
-      textStyleOfColor = "text-primary-dark";
+      styleOfBorderColor = "border-primary-dark";
+      styleOfTextColor = "text-primary-dark";
       break;
     }
     case "black": {
-      borderStyleOfColor = "border-black";
-      textStyleOfColor = "text-black";
+      styleOfBorderColor = "border-black";
+      styleOfTextColor = "text-black";
+      break;
+    }
+    case "accent": {
+      styleOfBorderColor = "border-accent";
+      styleOfTextColor = "text-accent";
       break;
     }
     default: {
-      borderStyleOfColor = "border-primary";
-      textStyleOfColor = "text-primary";
+      const wrongColor: never = color;
+      throw new Error(`${wrongColor as string} is wrong type.`);
     }
   }
 
   return (
-    <div className={`relative border px-[32px] py-[24px] ${borderStyleOfColor}`}>
+    <div className={`relative border px-[32px] py-[24px] ${styleOfBorderColor}`}>
       <div className="absolute left-0 top-0 inline-block -translate-y-1/2 bg-white px-[32px]">
-        <span className={`text-xl ${textStyleOfColor}`}>{title}</span>
+        <span className={`text-xl ${styleOfTextColor}`}>{title}</span>
       </div>
 
       <div>{lineBrokenTexts}</div>
