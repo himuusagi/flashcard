7e86bd945a5f8613afc66735533ef44ccf05972c
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-02 16:59:53 +0900

    new-flashcardページの作成

--- src/app/new-flashcard/page.tsx
+++ src/app/new-flashcard/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata, type NextPage } from "next";
-import { getServerSession } from "next-auth";
 import Main from "@/components/layouts/Main";
 import Heading1 from "@/components/elements/Heading1";
 import Inner from "@/components/layouts/Inner";
@@ -10,13 +9,7 @@ export const metadata: Metadata = {
   title: "単語帳の追加",
 };
 
-const Page: NextPage = async () => {
-  const session = await getServerSession();
-
-  if (!session?.user?.email) {
-    throw new Error("ユーザー情報が取得できませんでした。ログインし直してください。");
-  }
-
+const Page: NextPage = () => {
   return (
     <Main>
       <Heading1 title="単語帳の追加" />
