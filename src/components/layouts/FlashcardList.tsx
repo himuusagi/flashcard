3716d3618b6635eae2b3b02faca88dccaaebdf94
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-04 17:42:19 +0900

    FlashcardListコンポーネントの作成

---
+++ src/components/layouts/FlashcardList.tsx
@@ -0,0 +1,24 @@
+import { type FC } from "react";
+import Card from "./Card";
+import { type Flashcard } from "@/types/data";
+
+type Props = { flashcards: Flashcard[] };
+
+const FlashcardList: FC<Props> = ({ flashcards }) => {
+  return (
+    <ul className="flex flex-wrap">
+      {flashcards.map(({ id, title }) => {
+        return (
+          <li
+            key={id}
+            className="ml-[40px] mt-[40px] w-[calc((100%-_80px)_/_3)] [&:nth-child(-n+3)]:mt-0 [&:nth-child(3n+1)]:ml-0"
+          >
+            <Card title={title} href="" />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FlashcardList;
