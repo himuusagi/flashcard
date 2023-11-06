037013522146b2a2078c04b35922d777c94be51b
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-06 13:23:11 +0900

    LinkedTextコンポーネントの修正
    ポインターをカーソルにした

--- src/components/elements/LinkedText.tsx
+++ src/components/elements/LinkedText.tsx
@@ -11,7 +11,7 @@ const LinkedText: FC<Props> = ({ text, href, className }) => {
   return (
     <Link
       href={href}
-      className={`relative text-primary before:absolute before:bottom-0 before:left-0 before:block  before:h-[1px] before:w-full before:bg-transparent before:duration-100 hover:before:bg-primary focus:outline-primary-light ${className}`}
+      className={`relative cursor-pointer text-primary before:absolute before:bottom-0 before:left-0 before:block  before:h-[1px] before:w-full before:bg-transparent before:duration-100 hover:before:bg-primary focus:outline-primary-light ${className}`}
     >
       {text}
     </Link>
