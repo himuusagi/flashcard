7e851d3efe71a1a7ef74ee7bc7ad9b5f3c24cd44
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-09 19:02:51 +0900

    ClickableTextコンポーネントの作成

--- src/components/elements/ClickableText.tsx
+++ src/components/elements/ClickableText.tsx
@@ -10,7 +10,7 @@ type Props = {
 const ClickableText: FC<Props> = ({ text, onClick, formAction, className }) => {
   return (
     <button
-      type="button"
+      type="submit"
       onClick={onClick}
       formAction={formAction}
       className={`relative cursor-pointer leading-[19px] text-primary before:absolute before:bottom-0 before:left-0 before:block  before:h-[1px] before:w-full before:bg-transparent before:duration-100 hover:before:bg-primary focus:outline-primary-light ${className}`}
