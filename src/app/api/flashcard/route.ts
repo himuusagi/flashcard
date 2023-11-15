0b01c29385b6b25a0b9255afa20440426ec505fd
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-15 18:03:03 +0900

    `/api/flashcard/route.ts`にPATCHメソッドを作成

--- src/app/api/flashcard/route.ts
+++ src/app/api/flashcard/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { getServerSession } from "next-auth";
 import { addFlashcard } from "@/utils/add-flashcard";
+import { editFlashcard } from "@/utils/edit-flashcard";
 import { titleValidation } from "@/utils/validation/title-validation";
 
 export const POST = async (req: NextRequest) => {
@@ -21,6 +22,23 @@ export const POST = async (req: NextRequest) => {
 
     await addFlashcard(userId, title);
     return NextResponse.json({ message: "データが正常に追加されました" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "サーバー内部でエラーが発生しました" }, { status: 500 });
+  }
+};
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const body = (await req.json()) as { id: number; title: string };
+    const { id, title } = body;
+
+    const titleValidationError = titleValidation(title);
+    if (titleValidationError) {
+      return NextResponse.json({ message: "バリデーションエラーが発生しました" }, { status: 422 });
+    }
+
+    await editFlashcard(id, title);
+    return NextResponse.json({ message: "データが正常に更新されました" }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ message: "サーバー内部でエラーが発生しました" }, { status: 500 });
   }
