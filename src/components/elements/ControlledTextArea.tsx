cbe8a575ea4027cc86c865b02956cb2e3fe14162
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-16 18:24:46 +0900

    ControlledTextAreaコンポーネントの修正
    初期描画時に`useLayoutEffect`でtextareaの高さを値に応じて変更するようにした
    textareaの値の変更ごとにtextareaの高さを変更するのを`onChange`でやっていたのを`useLayoutEffect`で行うようにした

--- src/components/elements/ControlledTextArea.tsx
+++ src/components/elements/ControlledTextArea.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, type ChangeEventHandler, type FocusEventHandler } from "react";
+import { useRef, useLayoutEffect, type FocusEventHandler } from "react";
 import { useController, type UseControllerProps, type FieldValues } from "react-hook-form";
 
 type Props<T extends FieldValues> = UseControllerProps<T> & {
@@ -35,19 +35,18 @@ const ControlledTextArea = <T extends FieldValues>(props: Props<T>) => {
     }
   }
 
+  const { field } = useController<T>({ name, control, rules });
+
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const spanRef = useRef<HTMLSpanElement | null>(null);
 
-  const { field } = useController<T>({ name, control, rules });
-
-  const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    field.onChange(e);
+  useLayoutEffect(() => {
     if (textareaRef.current) {
       const textareaStyle = textareaRef.current.style;
       textareaStyle.height = "auto";
       textareaStyle.height = `${textareaRef.current.scrollHeight}px`;
     }
-  };
+  }, [textareaRef, field.value]);
 
   const onBlur: FocusEventHandler<HTMLTextAreaElement> = (e) => {
     const value = e.target.value;
@@ -66,7 +65,6 @@ const ControlledTextArea = <T extends FieldValues>(props: Props<T>) => {
         {...field}
         ref={textareaRef}
         placeholder={placeholder}
-        onChange={onChange}
         onBlur={onBlur}
         rows={1}
         className={`peer h-[32px] min-h-[32px] w-full resize-none overflow-hidden px-[32px] py-[4px] outline-none duration-150 focus:placeholder-transparent ${styleOfTextAreaColor}`}
