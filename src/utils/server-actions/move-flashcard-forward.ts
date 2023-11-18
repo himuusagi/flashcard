fa3150cfd4f8078cd915bd5c5dbebb5714866bf8
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-18 19:22:21 +0900

    moveFlashcardForward関数のリクエストの検証を修正

--- src/utils/server-actions/move-flashcard-forward.ts
+++ src/utils/server-actions/move-flashcard-forward.ts
@@ -4,23 +4,29 @@ import { revalidatePath } from "next/cache";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
 
-export const moveFlashcardForward = async (id: number, order: number, isFirst: boolean) => {
-  if (isFirst) {
-    throw new Error("リクエストが不正です");
-  }
-
+export const moveFlashcardForward = async (id: number) => {
   const session = await getServerSession();
-  if (!session?.user?.email) {
+  const userId = session?.user?.email;
+  if (!userId) {
     throw new Error("ユーザー情報が取得できませんでした");
   }
 
-  const userId = session.user.email;
+  const flashcard = await prisma.flash_Card.findUnique({ where: { userId, id } });
+  if (!flashcard) {
+    throw new Error("アクセスが禁止されているため、リクエストが拒否されました");
+  }
+
+  const isFirst = flashcard.order === 1;
+  if (isFirst) {
+    throw new Error("不正なリクエストです");
+  }
+
   await prisma.flash_Card.update({ where: { id }, data: { order: 0 } });
   await prisma.flash_Card.updateMany({
-    where: { userId, order: order - 1 },
+    where: { userId, order: flashcard.order - 1 },
     data: { order: { increment: 1 } },
   });
-  await prisma.flash_Card.update({ where: { id }, data: { order: order - 1 } });
+  await prisma.flash_Card.update({ where: { id }, data: { order: flashcard.order - 1 } });
 
   revalidatePath("/");
 };
