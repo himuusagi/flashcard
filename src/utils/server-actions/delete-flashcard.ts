8c22a1324765e7679ba6e2397e0df016e429235b
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-18 18:31:24 +0900

    deleteFlashcard関数のリクエストの検証を修正

--- src/utils/server-actions/delete-flashcard.ts
+++ src/utils/server-actions/delete-flashcard.ts
@@ -4,17 +4,22 @@ import { revalidatePath } from "next/cache";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
 
-export const deleteFlashcard = async (id: number, order: number) => {
+export const deleteFlashcard = async (id: number) => {
   const session = await getServerSession();
-  if (!session?.user?.email) {
-    throw new Error("ユーザー情報が取得できませんでした");
+  const userId = session?.user?.email;
+  if (!userId) {
+    throw new Error("認証が必要なため、リクエストが拒否されました");
   }
 
-  const userId = session.user.email;
+  const flashcard = await prisma.flash_Card.findUnique({ where: { userId, id } });
+  if (!flashcard) {
+    throw new Error("アクセスが禁止されているため、リクエストが拒否されました");
+  }
+
   await prisma.flash_Card.delete({ where: { id } });
 
   await prisma.flash_Card.updateMany({
-    where: { userId: userId, order: { gte: order } },
+    where: { userId: userId, order: { gte: flashcard.order } },
     data: {
       order: { decrement: 1 },
     },
