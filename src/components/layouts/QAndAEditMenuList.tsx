fc6daa8087a5ff1a73539b6123fdd93a2d7962f9
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-22 19:33:06 +0900

    QAndAEditMenuListコンポーネントの修正
    不要なプロップスの削除
    問題、解答の編集のリンクを変更
    "順番を前へ"、"順番を後ろへ"、"削除"ボタンをClickableTextコンポーネントに変更

--- src/components/layouts/QAndAEditMenuList.tsx
+++ src/components/layouts/QAndAEditMenuList.tsx
@@ -1,35 +1,24 @@
 import { type FC } from "react";
+import ClickableText from "../elements/ClickableText";
 import LinkedText from "../elements/LinkedText";
 
 type Props = {
-  editQuestionHref: string;
-  editAnswerHref: string;
-  orderForwardHref: string;
-  orderBackHref: string;
-  deleteQAndAHref: string;
+  flashcardId: number;
+  qaId: number;
 };
 
-const QAndAEditMenuList: FC<Props> = ({
-  editQuestionHref,
-  editAnswerHref,
-  orderForwardHref,
-  orderBackHref,
-  deleteQAndAHref,
-}) => {
+const QAndAEditMenuList: FC<Props> = ({ flashcardId, qaId }) => {
   return (
-    <ul className="">
+    <ul>
       <li>
-        <LinkedText text="問題文の編集" href={editQuestionHref} />
+        <LinkedText text="問題、解答の編集" href={`/flashcards/${flashcardId}/qa/${qaId}/edit`} />
       </li>
       <li className="mt-[4px]">
-        <LinkedText text="解答文の編集" href={editAnswerHref} />
+        <ClickableText text="順番を前へ" />
+        <ClickableText text="順番を後ろへ" className="ml-[28px]" />
       </li>
       <li className="mt-[4px]">
-        <LinkedText text="順番を前へ" href={orderForwardHref} />
-        <LinkedText text="順番を後ろへ" href={orderBackHref} className="ml-[28px]" />
-      </li>
-      <li className="mt-[4px]">
-        <LinkedText text="削除" href={deleteQAndAHref} />
+        <ClickableText text="削除" />
       </li>
     </ul>
   );
