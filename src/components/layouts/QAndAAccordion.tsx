a9625dcd36c4e78b117521d3a2bc72d67a8006c6
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-23 15:51:11 +0900

    QAndAAccordionコンポーネントの修正
    解答文のテキストの改行の仕方を変えた

--- src/components/layouts/QAndAAccordion.tsx
+++ src/components/layouts/QAndAAccordion.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  type FC,
-  type ReactNode,
-  type ReactElement,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import { type FC, type ReactNode, type Dispatch, type SetStateAction } from "react";
 
 type Props = {
   questionText: string;
@@ -24,15 +18,12 @@ const QAndAAccordion: FC<Props> = ({ questionText, answerText, isOpen, setIsOpen
     lineBrokenQuestionTexts.push(text);
   });
 
-  const lineBrokenAnswerTexts: ReactElement[] = answerText.split(/\n/).map((text, index) => {
-    return (
-      <p
-        key={index}
-        className={`duration-300 ${isOpen ? "mt-2 leading-[1.5] first:mt-0" : "mt-0 leading-[0]"}`}
-      >
-        {text}
-      </p>
-    );
+  const lineBrokenAnswerTexts: ReactNode[] = [];
+  answerText.split(/\n/).forEach((text, index) => {
+    if (index !== 0) {
+      lineBrokenAnswerTexts.push(<br key={index} />);
+    }
+    lineBrokenAnswerTexts.push(text);
   });
 
   return (
@@ -46,7 +37,7 @@ const QAndAAccordion: FC<Props> = ({ questionText, answerText, isOpen, setIsOpen
           isOpen ? "mt-[12px] leading-[1.5] opacity-100" : "mt-0 leading-[0] opacity-0"
         }`}
       >
-        {lineBrokenAnswerTexts}
+        <p>{lineBrokenAnswerTexts}</p>
       </div>
     </div>
   );
