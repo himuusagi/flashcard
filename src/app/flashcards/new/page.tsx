54eb86113a549ea45b4785bc1275886551f008c4
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-27 18:29:50 +0900

    単語帳の追加ページの修正
    認証済みでなければエラーを出すようにした

--- src/app/flashcards/new/page.tsx
+++ src/app/flashcards/new/page.tsx
@@ -1,15 +1,21 @@
-import { Metadata, type NextPage } from "next";
-import Main from "@/components/layouts/Main";
+import { type Metadata, type NextPage } from "next";
+import { getUserId } from "@/utils/get-user-id";
+import AddFlashcardForm from "@/components/layouts/AddFlashcardForm";
+import ContentWrapper from "@/components/layouts/ContentWrapper";
 import Heading1 from "@/components/elements/Heading1";
 import Inner from "@/components/layouts/Inner";
-import ContentWrapper from "@/components/layouts/ContentWrapper";
-import AddFlashcardForm from "@/components/layouts/AddFlashcardForm";
+import Main from "@/components/layouts/Main";
 
 export const metadata: Metadata = {
   title: "単語帳の追加",
 };
 
-const Page: NextPage = () => {
+const Page: NextPage = async () => {
+  const userId = await getUserId();
+  if (!userId) {
+    throw new Error("認証が必要なため、リクエストが拒否されました");
+  }
+
   return (
     <Main>
       <Heading1 title="単語帳の追加" />
