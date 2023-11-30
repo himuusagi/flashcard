c0cf2d70cde68e9ea21bfa05e0fce19f995201ed
MizukiNakamura <himuusagi1127@gmail.com>
2023-11-30 18:19:05 +0900

    フラッシュカードの順番の変更と削除の関数の修正
    送信のステータスとメッセージをreturnするようにした

--- src/utils/server-actions/delete-flashcard.ts
+++ src/utils/server-actions/delete-flashcard.ts
@@ -1,31 +1,37 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
+import { getUserId } from "../get-user-id";
 
-export const deleteFlashcard = async (id: number) => {
-  const session = await getServerSession();
-  const userId = session?.user?.email;
-  if (!userId) {
-    throw new Error("認証が必要なため、リクエストが拒否されました");
+export const deleteFlashcard = async (
+  id: number
+): Promise<{ success: boolean; message: string }> => {
+  try {
+    const userId = await getUserId();
+    if (!userId) {
+      return { success: false, message: "認証が必要なため、リクエストが拒否されました" };
+    }
+
+    const flashcard = await prisma.flash_Card.findUnique({ where: { userId, id } });
+    if (!flashcard) {
+      return { success: false, message: "リクエストしたリソースが見つかりません" };
+    }
+
+    await prisma.question_Answer.deleteMany({ where: { flashCardId: id } });
+    await prisma.flash_Card.delete({ where: { id } });
+    await prisma.flash_Card.updateMany({
+      where: { userId: userId, order: { gte: flashcard.order } },
+      data: {
+        order: { decrement: 1 },
+      },
+    });
+    revalidatePath("/");
+    return { success: true, message: "データが削除されました" };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, message: error.message };
+    }
+    return { success: false, message: "サーバーで不明なエラーが発生しました" };
   }
-
-  const flashcard = await prisma.flash_Card.findUnique({ where: { userId, id } });
-  if (!flashcard) {
-    throw new Error("アクセスが禁止されているため、リクエストが拒否されました");
-  }
-
-  await prisma.question_Answer.deleteMany({ where: { flashCardId: id } });
-
-  await prisma.flash_Card.delete({ where: { id } });
-
-  revalidatePath("/");
-
-  await prisma.flash_Card.updateMany({
-    where: { userId: userId, order: { gte: flashcard.order } },
-    data: {
-      order: { decrement: 1 },
-    },
-  });
 };
