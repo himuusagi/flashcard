13e4c4df6730978b19bfcd64698017bf5b07f032
MizukiNakamura <himuusagi1127@gmail.com>
2023-12-07 17:52:57 +0900

    TestScoreコンポーネントの修正
    半角数字を全角数字で表示されるようにした

--- src/components/elements/TestScore.tsx
+++ src/components/elements/TestScore.tsx
@@ -1,22 +1,25 @@
 import { type FC } from "react";
+import { getFullWithNumber } from "@/utils/get-full-width-number";
 
 type Props = {
   totalNumberOfQuestions: number;
   numberOfCorrectAnswers: number;
-  numberOfWrongAnswers: number;
+  numberOfIncorrectAnswers: number;
 };
 
 const TestScore: FC<Props> = ({
   totalNumberOfQuestions,
   numberOfCorrectAnswers,
-  numberOfWrongAnswers,
+  numberOfIncorrectAnswers,
 }) => {
   return (
     <div className="text-center text-xl">
-      <span className="text-primary">{numberOfCorrectAnswers}問正解</span>
-      <span className="ml-[8px] text-gray">{numberOfWrongAnswers}問不正解</span>
+      <span className="text-primary">{getFullWithNumber(numberOfCorrectAnswers)}問正解</span>
+      <span className="ml-[8px] text-gray">
+        {getFullWithNumber(numberOfIncorrectAnswers)}問不正解
+      </span>
       <span className="ml-[8px]">/</span>
-      <span className="ml-[8px]">{totalNumberOfQuestions}問中</span>
+      <span className="ml-[8px]">{getFullWithNumber(totalNumberOfQuestions)}問中</span>
     </div>
   );
 };
