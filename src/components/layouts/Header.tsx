c31f369cef29e2910090162e692b8e868c184b72
MizukiNakamura <himuusagi1127@gmail.com>
2023-12-13 16:31:19 +0900

    Headerコンポーネントの修正
    LinkedTextコンポーネントのサイズをmediumにした
    headerタグの仕切りの横線を入れた

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -5,23 +5,29 @@ import SignoutButton from "../elements/SignoutButton";
 
 const Header: FC = () => {
   return (
-    <header className="fixed left-0 top-0 z-10 w-full bg-white shadow-sm shadow-primary-light">
+    <div className="fixed left-0 top-0 z-10 w-full">
+      <header className="bg-white">
+        <Inner width="wide">
+          <nav>
+            <ul className="flex h-[50px] items-center justify-end">
+              <li>
+                <LinkedText text="TOP" href="/" size="medium" />
+              </li>
+              <li className="ml-[20px]">
+                <LinkedText text="単語帳の追加" href="/flashcards/new" size="medium" />
+              </li>
+              <li className="ml-[20px]">
+                <SignoutButton />
+              </li>
+            </ul>
+          </nav>
+        </Inner>
+      </header>
+
       <Inner width="wide">
-        <nav>
-          <ul className="flex h-[40px] items-center justify-end">
-            <li>
-              <LinkedText text="TOP" href="/" size="large" />
-            </li>
-            <li className="ml-[20px]">
-              <LinkedText text="単語帳の追加" href="/flashcards/new" size="large" />
-            </li>
-            <li className="ml-[20px]">
-              <SignoutButton />
-            </li>
-          </ul>
-        </nav>
+        <hr className="h-[2px] rounded-full bg-gray-light" />
       </Inner>
-    </header>
+    </div>
   );
 };
 
