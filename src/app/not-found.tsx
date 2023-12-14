c5959bfd96678b6933dc7124e4a90b149c377d4e
MizukiNakamura <himuusagi1127@gmail.com>
2023-12-14 16:11:53 +0900

    not-foundページの修正
    ヘッダーが表示されるようにした

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,23 +3,28 @@ import ContentWrapper from "@/components/layouts/ContentWrapper";
 import Inner from "@/components/layouts/Inner";
 import LinkedText from "@/components/elements/LinkedText";
 import Main from "@/components/layouts/Main";
+import Header from "@/components/layouts/Header";
 
 const NotFound = () => {
   return (
-    <Main hasHeader={false}>
-      <Inner width="narrow">
-        <ContentWrapper>
-          <BoxWithTitle
-            title="404 Not Found"
-            text="申し訳ありませんが、リクエストされたページは見つかりませんでした。"
-            color="primary"
-          />
-          <div className="mt-[32px] text-center">
-            <LinkedText text="TOP" href="/" />
-          </div>
-        </ContentWrapper>
-      </Inner>
-    </Main>
+    <>
+      <Header />
+
+      <Main hasHeader={true}>
+        <Inner width="narrow">
+          <ContentWrapper>
+            <BoxWithTitle
+              title="404 Not Found"
+              text="申し訳ありませんが、リクエストされたページは見つかりませんでした。"
+              color="primary"
+            />
+            <div className="mt-[32px] text-center">
+              <LinkedText text="TOP" href="/" />
+            </div>
+          </ContentWrapper>
+        </Inner>
+      </Main>
+    </>
   );
 };
 
